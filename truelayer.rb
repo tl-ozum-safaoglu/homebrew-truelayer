1d2cb24af2b1d1d0ee1ff2bd72d7e5c555fc2fcd
Ozum Safaoglu <ozum.safaoglu@truelayer.com>
2022-07-14 20:43:56 +0100

    Package update

--- truelayer.rb
+++ truelayer.rb
@@ -2,7 +2,7 @@ class Truelayer < Formula
   desc "Truelayer CLI"
   homepage "https://github.com/tl-ozum-safaoglu/truelayer-cli"
   url "https://github.com/tl-ozum-safaoglu/truelayer-cli/releases/download/v0.0.1/truelayer.tar.gz"
-  sha256 "f6892ac007923324ba5d9ab91e4cd7a368fb1a30c69399d073590e48ea118be7"
+  sha256 "8c1b2e0f38cf541ec317910d8d370a5304af63e986737ccbdee1534697080c74"
   version "0.0.1"
 
   def install
